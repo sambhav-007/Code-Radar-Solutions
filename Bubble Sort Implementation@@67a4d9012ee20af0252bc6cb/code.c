bb8979803d6b22830f474bbc7f0f28eae92acd62
sambhav-007 <sambhav.sehgal.007@gmail.com>
2025-02-10 10:44:19 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
-
 int main() {
-    int n;
-    scanf("%d", &n);
+    int n,temp;
+    scanf("%d",&n);
     int a[n];
-
-    // Taking input
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
+    for (int i =0; i<n; i++){
+        scanf("%d",&a[i]);
     }
-
-    // Optimized Bubble Sort
-    for (int i = 0; i < n - 1; i++) {
-        int swapped = 0;  // Track if any swaps happened in this pass
-        for (int j = 0; j < n - i - 1; j++) {
-            if (a[j] > a[j + 1]) {
-                int temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
-                swapped = 1;  // Swap occurred
+    for (int i=1; i<n;i++){
+        for(int j=0;j<n-i;j++){
+            if(a[j]>a[j+1]){
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
             }
         }
-        if (!swapped) break;  // Stop if no swaps occurred
     }
-
-    // Printing sorted array
-    for (int i = 0; i < n; i++) {
-        printf("%d ", a[i]);
+    for (int i =0; i<n; i++){
+        printf("%d ",a[i]);
     }
-
     return 0;
 }
