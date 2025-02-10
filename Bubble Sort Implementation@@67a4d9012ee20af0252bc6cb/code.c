5586f8cea1665f532f6fbbbe3090cc0ddb4fff5b
sambhav-007 <sambhav.sehgal.007@gmail.com>
2025-02-10 11:04:25 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>  // Needed for malloc (only if using dynamic arrays)
 
-// Function to perform Bubble Sort
+// Bubble Sort function
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -26,17 +27,22 @@ int main() {
     int n;
 
     // Read the number of elements
-    scanf("%d", &n);
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        printf("Invalid input\n");
+        return 1;
+    }
 
-    // Declare an array
-    int arr[n];
+    int arr[n]; // This works in C99+, else use malloc
 
     // Read array elements
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        if (scanf("%d", &arr[i]) != 1) {
+            printf("Invalid input\n");
+            return 1;
+        }
     }
 
-    // Perform Bubble Sort
+    // Sort the array
     bubbleSort(arr, n);
 
     // Print the sorted array
