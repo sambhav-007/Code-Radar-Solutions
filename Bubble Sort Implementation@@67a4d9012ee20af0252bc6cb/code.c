8152f11595c94dda0668f430dfd63bf1493eb36a
sambhav-007 <sambhav.sehgal.007@gmail.com>
2025-02-12 05:29:26 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -22,31 +22,3 @@ void printArray(int arr[], int n) {
     }
     printf("\n");
 }
-
-int main() {
-    int n;
-
-    // Read the number of elements
-    if (scanf("%d", &n) != 1 || n <= 0) {
-        printf("Invalid input\n");
-        return 1;
-    }
-
-    int arr[n]; // This works in C99+, else use malloc
-
-    // Read array elements
-    for (int i = 0; i < n; i++) {
-        if (scanf("%d", &arr[i]) != 1) {
-            printf("Invalid input\n");
-            return 1;
-        }
-    }
-
-    // Sort the array
-    bubbleSort(arr, n);
-
-    // Print the sorted array
-    printArray(arr, n);
-
-    return 0;
-}
