b3c5b8ff27226560c947c8eb1ac323cf4c08742d
sambhav-007 <sambhav.sehgal.007@gmail.com>
2025-02-24 05:51:26 +0530

    Solution to Calculate Average Marks of Students

--- Calculate Average Marks of Students@@67877d61dc107486ad5c2743/code.c
+++ Calculate Average Marks of Students@@67877d61dc107486ad5c2743/code.c
@@ -18,7 +18,7 @@ int main() {
         scanf("%d %s %f", &students[i].roll_number, students[i].name, &students[i].marks);
         sum+=students[i].marks;
     }
-    printf("Average Marks: %.2f",sum/N);
+    printf("Average Marks: %.2f",(float)sum/N);
 
     return 0;
 }
